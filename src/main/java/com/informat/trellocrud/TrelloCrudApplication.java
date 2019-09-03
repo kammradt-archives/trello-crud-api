371367be209a67519dc550f1c4baaf304c9c73a6
kammradt <vinicius.kammradt1@gmail.com>
2019-09-02 23:13:05 -0300

    :tada: First commit
    Creating project structure using start.spring.io

---
+++ src/main/java/com/informat/trellocrud/TrelloCrudApplication.java
@@ -0,0 +1,13 @@
+package com.informat.trellocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloCrudApplication.class, args);
+	}
+
+}
