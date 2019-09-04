415649957f6bd10e0c62d61167d97fa5abe2a1b8
kammradt <vinicius.kammradt1@gmail.com>
2019-09-04 00:08:20 -0300

    :wrench: Adding additional configuration - Heroku #3

--- src/main/java/com/informat/trellocrud/TrelloCrudConfig.java
+++ src/main/java/com/informat/trellocrud/TrelloCrudConfig.java
@@ -11,5 +11,6 @@ public class TrelloCrudConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+
     }
 }
